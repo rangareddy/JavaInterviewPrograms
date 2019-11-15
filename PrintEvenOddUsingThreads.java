d85c25cf07b28e4a9dabf378a21c7fabc18485c7
Ranga Reddy <rangareddy.avula@gmail.com>
2019-11-15 19:26:25 +0530

    Create PrintEvenOddUsingThreads.java

---
+++ PrintEvenOddUsingThreads.java
@@ -0,0 +1,77 @@
+
+/**
+ * User: Ranga Reddy
+ * Date: 11/15/2019
+ * Time: 3:38 PM
+ * Description:
+ */
+
+class EvenOrOdd implements Runnable {
+
+    private Printer printer;
+    private int max;
+    private boolean isEvenNumber;
+
+    public EvenOrOdd(Printer printer, int max, boolean isEvenNumber) {
+        this.printer = printer;
+        this.max = max;
+        this.isEvenNumber = isEvenNumber;
+    }
+
+    public void run() {
+        int number = isEvenNumber ? 2 : 1;
+        while (number <= max) {
+            if (isEvenNumber) {
+                printer.printEven(number);
+            } else {
+                printer.printOdd(number);
+            }
+            number += 2;
+        }
+    }
+}
+
+class Printer {
+
+    private volatile boolean isOdd;
+
+    synchronized void printEven(int number) {
+        while (!isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        //System.out.println(Thread.currentThread().getName() + ":" + number);
+        System.out.println(number);
+        isOdd = false;
+        notify();
+    }
+
+    synchronized void printOdd(int number) {
+        while (isOdd) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        //System.out.println(Thread.currentThread().getName() + ":" + number);
+        System.out.println(number);
+        isOdd = true;
+        notify();
+    }
+}
+
+public class PrintEvenOddUsingThreads {
+    public static void main(String[] args) {
+        Printer printer = new Printer();
+        int maxNum = 10;
+        boolean isEvenNumber = true;
+        Thread oddThread = new Thread(new EvenOrOdd(printer, maxNum, !isEvenNumber), "Odd");
+        Thread evenThread = new Thread(new EvenOrOdd(printer, maxNum, isEvenNumber), "Even");
+        oddThread.start();
+        evenThread.start();
+    }
+}
